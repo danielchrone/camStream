f14d7c07d0865ea9e85feb994b7f909b38bc90eb
DanielChrone <danielchrone@icloud.com>
2018-10-26 11:54:54 +0200

    test push

--- test.py
+++ test.py
@@ -1,6 +1,17 @@
-colors = ["red", "yellow", "green", "blue"]
+import cv2
 
-i = 0
-while i < len(colors):
-    print(i, colors[i])
-i = i + 1
+cv2.namedWindow("preview")
+vc = cv2.VideoCapture(0)
+
+if vc.isOpened(): # try to get the first frame
+    rval, frame = vc.read()
+else:
+    rval = False
+
+while rval:
+    cv2.imshow("preview", frame)
+    rval, frame = vc.read()
+    key = cv2.waitKey(20)
+    if key == 27: # exit on ESC
+        break
+cv2.destroyWindow("preview")
